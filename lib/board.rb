f08537995a54637dd3dab35cc1ed3fbc1c1c1419
d4tk1dmVn <datkidmun@gmail.com>
2026-01-24 22:43:20 +0100

    Update Board for out of bounds writes and writes when its full

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,11 @@
 class Board
   attr_reader :height, :width
+
   EMPTY_SPACE = :empty
   def initialize(height = 3, width = 3)
     @height = height
     @width = width
+    @empty_spaces = height * width
     @spaces = Array.new(height) { Array.new(width) { EMPTY_SPACE } }
   end
 
@@ -18,9 +20,16 @@ class Board
   end
 
   def []=(row, col, mark)
+    raise StandardError, 'Board is full' if full?
+    raise StandardError, 'Out of bounds space' unless legal_coords?(row, col)
     raise StandardError, "Can't occupy an occupied space" unless empty_space?(row, col)
 
+    @empty_spaces -= 1
     @spaces[row][col] = mark.to_sym
+  end
+
+  def full?
+    @empty_spaces.zero?
   end
 
   private
