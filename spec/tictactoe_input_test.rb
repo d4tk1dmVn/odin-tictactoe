ab7ae54837c77402cd33863697bc91462fec0f13
d4tk1dmVn <datkidmun@gmail.com>
2026-03-22 09:32:26 +0100

    Refactor mark testcases + player name testcases

--- spec/tictactoe_input_test.rb
+++ spec/tictactoe_input_test.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/tictactoe_input'
 
-def expect_reprompt(input_array, expected_result, board_state)
+def expect_reprompt_mark(input_array, expected_result, board_state)
   expect(ttt_input).to receive(:gets).exactly(2).times.and_return(*input_array)
   expect(ttt_input.mark(board_state)).to eq(expected_result)
 end
@@ -21,23 +21,35 @@ describe TicTacToeInput do
     end
     context 'when getting invalid input for marking the board' do
       it 'remprompts when input is a random string' do
-        expect_reprompt(%W[verybadinput\n 1\n], [0, 0], board_state)
+        expect_reprompt_mark(%W[verybadinput\n 1\n], [0, 0], board_state)
       end
       it 'remprompts when input is a positive numerical string outside of the 1-9 range' do
-        expect_reprompt(%W[42\n 1\n], [0, 0], board_state)
+        expect_reprompt_mark(%W[42\n 1\n], [0, 0], board_state)
       end
       it 'remprompts when input is zero' do
-        expect_reprompt(%W[0\n 1\n], [0, 0], board_state)
+        expect_reprompt_mark(%W[0\n 1\n], [0, 0], board_state)
       end
       it 'remprompts when input is a negative numerical string outside of the 1-9 range' do
-        expect_reprompt(%W[-1\n 1\n], [0, 0], board_state)
+        expect_reprompt_mark(%W[-1\n 1\n], [0, 0], board_state)
       end
       it 'remprompts when the number is a previously chosen number' do
-        expect_reprompt(%W[1\n 2\n], [0, 1], diminished_board)
+        expect_reprompt_mark(%W[1\n 2\n], [0, 1], diminished_board)
       end
       it 'returns nil when all valid numbers have been chosen' do
         expect(ttt_input.mark({})).to eq(nil)
       end
+    end
+  end
+  context 'when testing #player_name' do
+    let(:all_letters) { %w[AAA BBB CCC DDD EEE FFF GGG HHH III JJJ KKK LLL MMM NNN OOO PPP QQQ RRR SSS TTT UUU VVV WWW YYY XXX ZZZ] }
+    let(:reprompt_cases) { %w[toolong TOOLONG aaaa sh aa1 aa$ AAA] }
+    it 'can return alphabetic 3 charactered string for each anglosaxon character' do
+      expect(ttt_input).to receive(:gets).exactly(26).times.and_return(*all_letters)
+      expect(26.times.map { ttt_input.player_name }).to match_array(all_letters)
+    end
+    it 'reprompts when the player name is not an alphabetic 3 charactered string' do
+      expect(ttt_input).to receive(:gets).exactly(7).times.and_return(*reprompt_cases)
+      expect(ttt_input.player_name).to eq('AAA')
     end
   end
 end
