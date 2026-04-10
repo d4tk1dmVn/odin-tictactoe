33eadeade34c31f3fc6faf87c45c8f95c84e0b77
d4tk1dmVn <datkidmun@gmail.com>
2026-04-10 20:19:02 +0200

    Change #winner implementation, refactor class and testcases accordingly, added testcases

--- lib/tictactoe_arbiter.rb
+++ lib/tictactoe_arbiter.rb
@@ -3,22 +3,30 @@ require_relative 'arbiter'
 class TicTacToeArbiter
   include Arbiter
 
-  attr_accessor :winner
+  attr_reader :winner
 
   def initialize(board)
     @board = board
+    @winner = nil
   end
 
   def winner?
-    rows = @board.each_row
-    cols = @board.each_column
-    diagonals = @board.diagonals_at(1, 1)
-    exes = %w[X X X]
-    os = %w[O O O]
-    [rows, cols, diagonals].any? { |arr| arr.include?(exes) || arr.include?(os) }
+    exes = triad_wins?(%w[X X X])
+    os = triad_wins?(%w[O O O])
+    @winner = exes ? 'X' : (os ? 'O' : nil)
+    exes || os
   end
 
   def tie?
-    !winner? && @board.full?
+    winner.nil? && @board.full?
+  end
+
+  private
+
+  def triad_wins?(triad)
+    rows = @board.each_row
+    cols = @board.each_column
+    diagonals = @board.diagonals_at(1, 1)
+    [rows, cols, diagonals].any? { |arr| arr.include?(triad) }
   end
 end
--- spec/tictactoe_arbiter_test.rb
+++ spec/tictactoe_arbiter_test.rb
@@ -9,6 +9,11 @@ def allow_board_scenario(rows)
   allow(board).to receive(:diagonals_at).with(1, 1).and_return([left_diag, right_diag])
 end
 
+def expect_winner(expected_winner)
+  expect(arbiter.winner?).to be true
+  expect(arbiter.winner).to eq expected_winner
+end
+
 describe TicTacToeArbiter do
   let(:board) { instance_double('board') }
   subject(:arbiter) { described_class.new(board) }
@@ -19,6 +24,7 @@ describe TicTacToeArbiter do
     end
     it 'there is no winner' do
       expect(arbiter.winner?).to be false
+      expect(arbiter.winner).to be nil
     end
     it 'there is no tie' do
       expect(arbiter.tie?).to be false
@@ -31,6 +37,7 @@ describe TicTacToeArbiter do
     end
     it 'there is no winner' do
       expect(arbiter.winner?).to be false
+      expect(arbiter.winner).to be nil
     end
     it 'there is a tie' do
       expect(arbiter.tie?).to be true
@@ -39,49 +46,57 @@ describe TicTacToeArbiter do
   context 'when checking for a winner on a board with an X winner by rows' do
     before { allow_board_scenario([%w[O O X], %w[X X X], %w[O X O]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'X'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a board with an O winner by rows' do
     before { allow_board_scenario([%w[O X X], %w[X X O], %w[O O O]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'O'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a bord with an X winner by columns' do
     before { allow_board_scenario([%w[X O O], %w[X O X], %w[X X O]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'X'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a bord with an O winner by columns' do
     before { allow_board_scenario([%w[X O O], %w[X O X], %w[O O X]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'O'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a bord with an X winner by left diagonal' do
     before { allow_board_scenario([%w[X O O], %w[X X O], %w[O O X]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'X'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a bord with an O winner by left diagonal' do
     before { allow_board_scenario([%w[O X X], %w[O O X], %w[X X O]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'O'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a bord with an X winner by right diagonal' do
     before { allow_board_scenario([%w[O O X], %w[O X O], %w[X X O]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'X'
+      expect_winner(expected_winner)
     end
   end
   context 'when checking for a winner on a bord with an O winner by left diagonal' do
     before { allow_board_scenario([%w[X O O], %w[X O X], %w[O X O]]) }
     it 'there is a winner' do
-      expect(arbiter.winner?).to be true
+      expected_winner = 'O'
+      expect_winner(expected_winner)
     end
   end
 end
